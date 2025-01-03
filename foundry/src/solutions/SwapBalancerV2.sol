38a3155cdf85bc146c439ad2b16d140624c6411f
t4sk <tsk.nakamura@gmail.com>
2025-01-03 15:14:19 +0900

    natspec

--- foundry/src/solutions/SwapBalancerV2.sol
+++ foundry/src/solutions/SwapBalancerV2.sol
@@ -11,11 +11,22 @@ import {
     BALANCER_POOL_ID_RETH_WETH
 } from "../Constants.sol";
 
+/// @title SwapBalancerV2
+/// @notice This contract facilitates swaps between rETH and WETH using Balancer V2.
+/// @dev The contract interacts with the Balancer Vault for token swaps.
 contract SwapBalancerV2 {
     IRETH constant reth = IRETH(RETH);
     IERC20 constant weth = IERC20(WETH);
     IVault constant vault = IVault(BALANCER_VAULT);
 
+    /// @notice Executes a token swap using the Balancer Vault.
+    /// @param tokenIn The address of the token to swap from.
+    /// @param tokenOut The address of the token to swap to.
+    /// @param amountIn The amount of `tokenIn` to be swapped.
+    /// @param amountOutMin The minimum amount of `tokenOut` to be received.
+    /// @param poolId The Balancer pool ID for the swap.
+    /// @return amountOut The amount of `tokenOut` received from the swap.
+    /// @dev Uses the `GIVEN_IN` swap kind to specify the input token amount.
     function swap(
         address tokenIn,
         address tokenOut,
@@ -43,6 +54,10 @@ contract SwapBalancerV2 {
         });
     }
 
+    /// @notice Swaps WETH to rETH using the Balancer Vault.
+    /// @param wethAmountIn The amount of WETH to be swapped.
+    /// @param rEthAmountOutMin The minimum amount of rETH to receive.
+    /// @dev The caller must approve the contract to transfer WETH on their behalf.
     function swapWethToReth(uint256 wethAmountIn, uint256 rEthAmountOutMin)
         external
     {
@@ -57,6 +72,10 @@ contract SwapBalancerV2 {
         );
     }
 
+    /// @notice Swaps rETH to WETH using the Balancer Vault.
+    /// @param rEthAmountIn The amount of rETH to be swapped.
+    /// @param wethAmountOutMin The minimum amount of WETH to receive.
+    /// @dev The caller must approve the contract to transfer rETH on their behalf.
     function swapRethToWeth(uint256 rEthAmountIn, uint256 wethAmountOutMin)
         external
     {
